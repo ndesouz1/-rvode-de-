16aad919bd8ed7865284377e12c272a5b02daa97
Nayara de Souza <54290219+ndesouz1@users.noreply.github.com>
2022-01-16 19:51:13 -0300

    Create arvore.java

---
+++ arvore.java
@@ -0,0 +1,12 @@
+
+public class Tree {
+  public static void main(String[] args){
+    System.out.println("Hi, I am Nayara");
+    System.out.println("I am going to plant a tree today");
+
+    System.out.println("   # ");
+    System.out.println("  ### ");
+    System.out.println(" ###### ");
+    System.out.println("######### ");
+  }
+}
